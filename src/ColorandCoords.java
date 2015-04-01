891073486517b2c3d4d1b084f24f27a22a3e0ca2
kschlorff16 <12kschlorff@gmail.com>
2015-04-01 00:31:11 -0500

    Final commit
    For now.

--- src/ColorandCoords.java
+++ src/ColorandCoords.java
@@ -1,27 +1,43 @@
+/** A fun wrapper class to contain specific colors and their respective frequencies.
+ *  
+ *  @author KentSchlorff
+ * 
+ * */
+ 
+
 import java.awt.Color;
 import java.util.ArrayList;
 
 
 
-// A fun wrapper class to contain specific colors and their respective frequencies. 
+ 
 
 
 public class ColorandCoords implements Comparable<ColorandCoords> {
 	private ArrayList<Integer> Xcoords = new ArrayList<Integer>();
 	private ArrayList<Integer> Ycoords = new ArrayList<Integer>();
 	private Color color;
-	private String hexTitle;
 	private int frequency = 0;
 
 
+	/**
+	 * Here's the constructor. There is only one.
+	 * @param x The x coordinate of the pixel.
+	 * @param y The y coordinate of the pixel.
+	 * @param newcolor The color of the pixel.
+	 */
 	public ColorandCoords(int x, int y, Color newcolor) {
 		this.Xcoords.add(x);
 		this.Ycoords.add(y);
 		this.color = newcolor;
-		this.hexTitle = convertColor(newcolor);
 		frequency++;
 	}
 	
+	/**
+	 * I wrote my own method to convert Colors to Hex Strings. There is a method written into Java, but I wrote this first, and I'm stubborn.
+	 * @param color The color to convert.
+	 * @return A String representation of the color in hexadecimal RGB format.
+	 */
 	public static String convertColor(Color color) {
 		
 			int r = color.getRed();
@@ -39,34 +55,58 @@ public class ColorandCoords implements Comparable<ColorandCoords> {
 	}
 	
 
-
+/**
+ * This method adds a new location of a particular color.
+ * @param x The x coordinate of the pixel.
+ * @param y The y coordinate of the pixel.
+ */
 	public void add(int x, int y) {
 		this.Xcoords.add(x);
 		this.Ycoords.add(y);
 		frequency++;
 	}
 
-	
+	/**
+	 * A get method for x coordinates.
+	 * @param x The index of the x coordinate within the ArrayList.
+	 * @return The xth index in Xcoords.
+	 */
 	public int getX(int x) {
 		return Xcoords.get(x);
 	}
 	
+	/**
+	 * A get method for y coordinates.
+	 * @param y The index of the y coordinate within the ArrayList.
+	 * @return The yth index in Ycoords.
+	 */
 	public int getY(int y) {
 		return Ycoords.get(y);
 	}
 
 
-
+/**
+ * A method to return the frequency of a given color. Useful for debugging and displaying more information if we wanted to do that.
+ * @return The frequency of the specific color.
+ */
 	public int getFrequency() {
 		return this.frequency;
 	}
 
+	
+	/**
+	 * A method to return the color of a given ColorandCoords object.
+	 * @return The color of the object.
+	 */
 	public Color getColor() {
 		Color returnColor = this.color;
 		return returnColor;
 	}
 
 
+	/**
+	 * Because Comparable has to implement compareTo, I just had it compare the frequencies. Works great!
+	 */
 	@Override
 	public int compareTo(ColorandCoords other) {
 		int otherFrequency = other.getFrequency();
--- src/PictureMagic.java
+++ src/PictureMagic.java
@@ -1,10 +1,18 @@
+/** PictureMagic takes in a picture as an argument, and recreates that picture using the most common color first.
+ * @author Kent Schlorff
+ * 
+ */
+
+
+
+
 import java.awt.Color;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 
 
-public class PictureAlgorithm {
+public class PictureMagic {
 	public static void main(String[] args) {
 		Picture pic = new Picture(args[0]);
 		int width  = pic.width();
@@ -12,10 +20,8 @@ public class PictureAlgorithm {
 		
 		
 
-//		Color[] colors = new Color[width * height];
 		Color colors = new Color(0, 0, 0);
 		HashSet<Color> uniqueColors = new HashSet<Color>();
-//		ArrayList<Color> uniqueList = new ArrayList<Color>();
 		BinarySearchST<String, Integer> ST = new BinarySearchST<String, Integer>();
 		ArrayList<ColorandCoords> colorandcoords = new ArrayList<ColorandCoords>();
 		int j = 0;
@@ -30,40 +36,23 @@ public class PictureAlgorithm {
 				}
 				else {
 					int k = ST.get(ColorandCoords.convertColor(colors));
-//					for (int xx = 0; xx < colorandcoords.size(); xx++) {
-//						if (colorandcoords.get(xx).getColor() == colors) {
 							colorandcoords.get(k).add(x, y);
 							
 						
 					
 				}
 			}
-//			j++;
+
 		}
 		Collections.sort(colorandcoords);
 //		System.out.println("The first color has this many colors: " + colorandcoords.get(0).getFrequency());
 //		System.out.println("The second color has this many colors: " + colorandcoords.get(1).getFrequency());
-//		System.out.println("The first color has this many colors: " + colorandcoords.get(colorandcoords.size()-1).getFrequency());
-//		System.out.println("The first color has this many colors: " + colorandcoords.get(colorandcoords.size()-2).getFrequency());
+//		System.out.println("The last color has this many colors: " + colorandcoords.get(colorandcoords.size()-1).getFrequency());
+//		System.out.println("The second to last color has this many colors: " + colorandcoords.get(colorandcoords.size()-2).getFrequency());
 		System.out.println("There are " + uniqueColors.size() + " unique colors in the picture.");
+			
 		
 		
-//		int zz = 0;
-//		for (int x = 0; x < width; x++){
-//			for (int y = 0; y < height; y++) {
-//				colorandcoords[0].add(x, y); 
-//				the reason the above line will be weird is because every single 
-//				element of colorandcoords will be an object
-//			}
-//			
-//		}
-//		
-		
-		
-		
-		
-		
-//		int k = 0;
 		StdDraw.clear();
 		StdDraw.setXscale(0, width);
 		StdDraw.setYscale(0, height);
@@ -73,12 +62,7 @@ public class PictureAlgorithm {
 			for (int i = 0; i < colorandcoords.get(z).getFrequency(); i++) {
 
 				StdDraw.setPenColor(colorandcoords.get(z).getColor());
-//				System.out.println(i);
 				StdDraw.point(colorandcoords.get(z).getX(i), height - (colorandcoords.get(z).getY(i)));
-				//				k++;
-				//				
-				//			}
-
 			}
 		}
 
